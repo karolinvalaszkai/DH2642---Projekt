6e1265161c5a6db1404306c7c970b2ed6a491b51
Sabina Nordell <sabinanordell@Sabinas-MacBook-Pro.local>
2018-05-09 14:56:25 +0200

    s

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function handleClick()  {
+  console.log('this is:', this);
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Quizmania</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={handleClick}>
+            Click here
+        </button>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
