fef96c7ae2ade4f3f81dc03c4ee753764e5076ab
Sofia <sofiablo@kth.se>
2018-05-16 15:07:58 +0200

    första testpush

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import * as Ons from 'react-onsenui';
 import logo from './logo.svg';
 import './App.css';
+import { Link  } from 'react-router-dom'
+import Main from './Main'
+
+
+/*const LoginLink = () => (
+<Link to='/login'>Login</Link>
+)*/
 
 class App extends Component {
   render() {
@@ -8,14 +16,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Quizmania</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Link to='/login'>Login</Link>
+        <Main/>
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        Login page test
+      </div>
+    );
+  }
+}
+
+export default Login;
