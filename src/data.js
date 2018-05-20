6f72ff1e7deca33b894a642c5f96de37f12b082d
Axel Ekwall <axel@minimize.se>
2018-05-20 23:41:41 +0200

    All data in one place!

--- src/data.js
+++ src/data.js
@@ -1,52 +1,52 @@
 export default [
   {
-    question: 'What country does this look like?',
     answers: ['Sweden', 'USA', 'France', 'Italy'],
-    correct: 3
+    correct: 3,
+    coordinates: { lat: 48.858289, lng: 2.294261 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['Italy', 'Sweden', 'USA', 'China'],
-    correct: 2
+    correct: 2,
+    coordinates: { lat: 59.3470962, lng: 18.0724084 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['Spain', 'Canada', 'U.S.A.', 'Norway'],
-    correct: 3
+    correct: 3,
+    coordinates: { lat: 40.689806, lng: -74.044483 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['Greece', 'Cyprus', 'Italy', 'Croatia'],
-    correct: 3
+    correct: 3,
+    coordinates: { lat: 41.89, lng: 12.491944 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['India', 'Bali', 'Japan', 'Thailand'],
-    correct: 1
+    correct: 1,
+    coordinates: { lat: 27.174, lng: 78.0421 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['Denmark', 'Indonesia', 'Taiwan', 'China'],
-    correct: 4
+    correct: 4,
+    coordinates: { lat: 39.916667, lng: 116.396973 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['Denmark', 'Vatican City State', 'Moldavia', 'China'],
-    correct: 2
+    correct: 2,
+    coordinates: { lat: 41.901944, lng: 12.456944 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['Australia', 'Indonesia', 'Taiwan', 'China'],
-    correct: 1
+    correct: 1,
+    coordinates: { lat: -33.857197, lng: 151.21514 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['Denmark', 'Peru', 'Taiwan', 'China'],
-    correct: 2
+    correct: 2,
+    coordinates: { lat: -13.163056, lng: -72.545556 }
   },
   {
-    question: 'What country does this look like?',
     answers: ['India', 'China', 'Egypt', 'Croatia'],
-    correct: 3
+    correct: 3,
+    coordinates: { lat: 29.979175, lng: 31.134358 }
   }
 ];
